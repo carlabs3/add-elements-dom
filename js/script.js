a6775df8204a6f343109af180281dce579da2775
Carla Barceló Sàbat <carla@MacBook-Pro-de-Carla.local>
2025-11-04 18:02:48 +0100

    Challenge 2 done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
  // Aquí tu código
+
+
+const boton = document.getElementById('agregar');
+const lista = document.getElementById('lista');
+
+boton.addEventListener('click', function () {
+    const NuevoElemento = prompt('Añade un nuevo elemento:');
+    lista.insertAdjacentHTML ('beforeend', '<li>'+ NuevoElemento + '</li>');
+});
